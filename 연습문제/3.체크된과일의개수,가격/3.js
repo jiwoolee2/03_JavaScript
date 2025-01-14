010722843acf714a323137e718b2bc925e3a8aa9
jiwoolee2 <dlwldn1009@naver.com>
2025-01-14 17:19:05 +0900

    tired

--- 연습문제/3.체크된과일의개수,가격/3.js
+++ 연습문제/3.체크된과일의개수,가격/3.js
@@ -1,13 +1,20 @@
-const div = document.querySelectorAll(".row"); // 배열로 반환
+const divs = document.querySelectorAll(".row"); // 배열로 반환
 const checkbox = document.querySelectorAll("[type=checkbox]")
 const btn = document.querySelector("#btn"); // 버튼 요소
 const result = document.querySelector("#result") // 결과 반환 요소
-i=0
+
 btn.addEventListener("click",()=>{
-    while(i<3){
-        if(div.children[i]===checked){
-            result.innerText += div.children[2]+div.children[3].innerText
-        }
-        i++
-    } 
+
+    result.innerText="";
+    let sum = 0;
+
+    for(let div of divs){
+        if(div.children[0].checked === true){
+            result.innerText += 
+            " "+div.children[1].innerText+div.children[3].value+"개";
+
+            sum += div.children[2].innerText*Number(div.children[3].value)
+    }}
+    result.innerText += " 총합 "+sum;
+    
 })
